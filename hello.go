f5902a3c27715ad0fc7edc774a8ef9538abb3b14
Kevin Chiu <kschiu@andrew.cmu.edu>
2017-07-08 23:51:45 -0700

    Fetch and parse webpage

--- hello.go
+++ hello.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-  "fmt"
+  "fmt" // pkg used to print
+  "net/http" // pkg to get pages
+  "io/ioutil"
 )
 
 func main() {
-  fmt.Println("foo")
+  resp, err := http.Get("https://google.com")
+  // Expect that functions return two things:
+  // first - result from fn
+  // second - an error, if there is one
+
+  fmt.Println("Error fetching webpage:", err)
+
+  body, err := ioutil.ReadAll(resp.Body)
+  // resp.Body is a datastream that we need to convert
+  // into something we can parse, using ioutil
+
+  fmt.Println("Error reading body:", err)
+
+  fmt.Println(string(body))
+  // Cast body into string, from a bytearray
 }
