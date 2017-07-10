aa2f5aec5db1ec2302472787dbdd32e05b10eb28
Kevin Chiu <kschiu@andrew.cmu.edu>
2017-07-09 21:43:12 -0700

    Check if body contains text we are looking for Case insensitive compares method added

--- hello.go
+++ hello.go
@@ -4,10 +4,11 @@ import (
   "fmt" // pkg used to print
   "net/http" // pkg to get pages
   "io/ioutil"
+  "strings" // pkg help with string manipulation
 )
 
 func main() {
-  resp, err := http.Get("https://google.com")
+  resp, err := http.Get("https://www.sfoutsidelands.com/get-involved/volunteers/")
   // Expect that functions return two things:
   // first - result from fn
   // second - an error, if there is one
@@ -20,6 +21,12 @@ func main() {
 
   fmt.Println("Error reading body:", err)
 
-  fmt.Println(string(body))
+  fmt.Println(csContains(string(body), "Stay Tuned For More Info"))
   // Cast body into string, from a bytearray
+  // Then check if page says "stay tuned still"
+}
+
+// Case Insensitive Contains
+func csContains(a, b string) bool {
+  return strings.Contains(strings.ToUpper(a), strings.ToUpper(b))
 }
