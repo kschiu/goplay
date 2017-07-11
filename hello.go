ac15bc0471fdc529c2c3d535f55068da486ddba8
Kevin Chiu <kschiu@andrew.cmu.edu>
2017-07-10 22:00:52 -0700

    Added sending email using gmal smtp server

--- hello.go
+++ hello.go
@@ -3,11 +3,41 @@ package main
 import (
   "fmt" // pkg used to print
   "net/http" // pkg to get pages
+  "net/smtp" //pkg to send email
   "io/ioutil"
   "strings" // pkg help with string manipulation
 )
 
+
+const (
+  SMTP_SERVER = "smtp.gmail.com"
+)
+
+type Sender struct {
+  username  string
+  password  string
+}
+
+
 func main() {
+  sender := Sender{"EMAIL", "PASSWORD"}
+
+  recipients := []string{"EMAIL"}
+
+  body := getVolunteerStatus()
+
+  sender.sendMail(recipients, body)
+}
+
+// Helper methods
+
+
+// Case Insensitive Contains
+func csContains(a, b string) bool {
+  return strings.Contains(strings.ToUpper(a), strings.ToUpper(b))
+}
+
+func getVolunteerStatus() string {
   resp, err := http.Get("https://www.sfoutsidelands.com/get-involved/volunteers/")
   // Expect that functions return two things:
   // first - result from fn
@@ -26,22 +56,33 @@ func main() {
     fmt.Println("Error reading body:", err)
   }
 
-  // TODO: Make this send me an email or text
-
   // Cast body into string, from a bytearray
   // Then check if page says "stay tuned still"
   if csContains(string(body), "Stay Tuned For More Info") {
-    fmt.Println("Volunteering not open yet")
+    return "Volunteering not open yet"
   } else {
     // Do something to notify me to sign up
-    fmt.Println("Go Sign Up For Volunteering")
+    return "Go Sign Up For Volunteering"
   }
 }
 
-// Helper method
 
+func (sender Sender) sendMail(Recipient []string, body string) {
+  msg := getVolunteerStatus()
 
-// Case Insensitive Contains
-func csContains(a, b string) bool {
-  return strings.Contains(strings.ToUpper(a), strings.ToUpper(b))
+  // 587 is the port
+  err := smtp.SendMail(
+    SMTP_SERVER + ":587",
+    smtp.PlainAuth("", sender.username, sender.password, SMTP_SERVER),
+    sender.username,
+    Recipient,
+    []byte(msg))
+
+  // Cast msg into a bytearray
+
+  if err != nil {
+    fmt.Println("Err sending:", err)
+  } else {
+    fmt.Println("Sent Mail")
+  }
 }
